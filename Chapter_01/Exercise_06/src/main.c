01b6eb88285d6a1f2daf23a00ce7321d10f3cd54
nomadrussian <nomadrussian@gmail.com>
2021-11-17 23:14:50 +0400

    Just some calculations and %d in printf().

---
+++ Chapter_01/Exercise_06/src/main.c
@@ -0,0 +1,11 @@
+/* Exercise 6 from Chapter I.
+ * There're just some calculations and output. */
+
+#include <stdio.h>
+
+int main(void) {
+        int toes = 10;
+        printf("Initial: %d\nDoubled: %d\nSquared: %d\n", toes, toes << 1, toes * toes);
+        return 0;
+}
+
