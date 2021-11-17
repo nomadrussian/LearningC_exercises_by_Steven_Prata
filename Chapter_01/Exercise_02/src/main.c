f0783a8841f36f84737bce34003c6f568c2fcbb0
George <nomadrussian@gmail.com>
2021-11-17 19:49:36 +0400

    First two exercises.

---
+++ Chapter_01/Exercise_02/src/main.c
@@ -0,0 +1,16 @@
+/* Exercise 2 from Chapter I.
+ * The task is simple - just write your name and your adress. Of course, it's a joke,
+ * nobody wants to show his home adress :D
+ * That's the task: 
+ *
+ *  Напишите программу, выводящую ваше имя и адрес.
+ *
+ * So simple. I don't have to invent anything. Just keep it simple.                  */
+
+#include <stdio.h>
+
+int main(void) {
+        printf("The program was written by George Glebov. Saratov, Russia. 2021\n");
+        return 0;
+}
+
