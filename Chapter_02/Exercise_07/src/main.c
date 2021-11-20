3ef0ba9f54cd06cb4339c9aecb3c0205e30846b5
nomadrussian <nomadrussian@gmail.com>
2021-11-20 08:06:03 +0400

    Converting inches to centimetres.

---
+++ Chapter_02/Exercise_07/src/main.c
@@ -0,0 +1,19 @@
+/* Exercise 7 from Chapter II.
+ * Converting inches to centimetres.
+ */
+
+#include <stdio.h>
+
+#define INCH_SIZE 2.54f
+
+int main(void) {
+
+        float inches;
+
+        printf("Please, type in inches amount: ");
+        scanf("%f", &inches);
+        printf("%g\" = %gcm\n", inches, inches * INCH_SIZE);
+
+        return 0;
+}
+
