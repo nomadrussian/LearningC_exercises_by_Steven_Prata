b1e6a687da6ee6a434869ec87c5399b3e40adc4c
nomadrussian <nomadrussian@gmail.com>
2021-11-21 02:30:37 +0400

    Fixed an array overflow bug.

--- Chapter_04/Exercise_01/src/main.c
+++ Chapter_04/Exercise_01/src/main.c
@@ -4,16 +4,20 @@
  */
 
 #include <stdio.h>
-#define MAX_SIZE 24
+#define MAX_LENGTH 12
 
 int main(void) {
 
-        char first_name[MAX_SIZE], last_name[MAX_SIZE];
+        char first_name[MAX_LENGTH + 1], last_name[MAX_LENGTH + 1];
 
         printf("Пожалуйста, введите ваше имя:  ");
-        scanf("%s", first_name);
-        printf("Пожалуйста, введите фамилию:   ");
-        scanf("%s", last_name);
+        scanf("%12s", first_name);
+        
+        char ch;                                      // Clearing the buffer to fix a bug when the input
+        while((ch = getchar()) != '\n' && ch != EOF); // string lenght is greater than MAX_LENGTH.
+
+        printf("Пожалуйста, введите фамилию:   ");    // So, I made it 12 just to show that in case of string length > 12
+        scanf("%12s", last_name);                     // the program doesn't crash but just cuts the string.
 
         printf("Ваши данные: %s %s\n", last_name, first_name);
 
