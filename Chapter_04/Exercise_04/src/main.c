0b23b682aa288eb8c6b57711cde372d4ca9f6683
nomadrussian <nomadrussian@gmail.com>
2021-11-22 16:28:39 +0400

    Centimetres converting.

---
+++ Chapter_04/Exercise_04/src/main.c
@@ -0,0 +1,39 @@
+/* Exercise 4 from Chapter IV.
+ * Converting cantimetres to metres, feet and inches.
+ *
+ *      1 metre  =  100   centimetres
+ *      1 inch   =  2.52  centimetres
+ *      1 foot   =  12    inches
+ */
+
+#include <stdio.h>
+
+double cm_toInches(double num);
+double cm_toFeet(double num);
+double cm_toMetres(double num);
+
+int main(void) {
+
+        double height_cm;
+
+        printf("Please, type in your height in cm: ");
+        scanf("%lf", &height_cm);
+        printf("Your height in metres  :  %g\n", cm_toMetres(height_cm));
+        printf("Your height in inches  :  %g\n", cm_toInches(height_cm));
+        printf("Your height in feet    :  %g\n", cm_toFeet(height_cm));        
+
+        return 0;
+}
+
+double cm_toInches(double num) {
+        return num / 2.54;
+}
+
+double cm_toFeet(double num) {
+        return num / (2.54 * 12.0);
+}
+
+double cm_toMetres(double num) {
+        return num / 1.0e2;             //Just to show off :)
+}
+
