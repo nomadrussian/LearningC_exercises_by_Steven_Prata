50ce7fd732582236c40bfefbc0d1d13cabe4a9da
nomadrussian <nomadrussian@gmail.com>
2021-11-25 23:59:35 +0400

    Fixed. Now it has to work correctly in all cases.

--- Chapter_05/Exercise_04/src/main.c
+++ Chapter_05/Exercise_04/src/main.c
@@ -3,18 +3,20 @@
  */
 
 #include <stdio.h>
+#include <math.h>
 
 #define INCH 2.54
-#define FEET 12
+#define FOOT 30.48
 
 void clearInputBuffer(void);
 
 int main() {
 
-        double inches, centimetres;
+        double inches;
         int    feet;
+        double centimetres;
 
-        do {
+        while(1) {
                 printf("Please, type in the amount of centimetres. Give me negative or zero to exit: ");
                 int check = scanf("%lf", &centimetres);
 
@@ -28,14 +30,15 @@ int main() {
                         return 0;
                 }
                 
-                inches = centimetres / 2.54;
-                feet = (int)inches / 12.0;
-                inches -= feet * 12.0;
+                centimetres = roundf(centimetres * 1000) / 1000.0; //Capping precision at .3
 
-                printf("%.2f cm = %d feet %.2f inches.\n", centimetres, feet, inches);
+                feet = (int)(centimetres / FOOT);
+                inches = (centimetres - feet * FOOT) / INCH;
+
+                printf("%g cm = %d\' %g\"\n", centimetres, feet, inches);
+
                 clearInputBuffer();
-        
-        } while(centimetres > 0);
+        }
 
         return 0;
 }
@@ -44,4 +47,13 @@ void clearInputBuffer(void) {
         static char buffer;
         while((buffer = getchar()) != '\n' && buffer != EOF);
 }
+
+/* In the previous version low precision formatting such as %.2f led to delirious stuff like:
+ * 30.48 cm = 1' 0.00" but
+ * 30.47 cm = 0' 12.00" ????? :=/ looks weird. It took me a while to figure out how to fix it. 
+ * Actually e.g 30.47999999 is converted to inches, but due to the floats precision loss I had 0 feet
+ * and 12.0 inches.
+ * I used roundf() from math.h to cap centimetres variable's precision at 3 decimals after the point.
+ * That solved the problem. Now 30.47999999 turns into 30.479 and the rest works correctly.
+ */
 
