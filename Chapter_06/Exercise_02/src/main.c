a27c9654cd95d03f9ddbb9f0eab6f0da337f144b
nomadrussian <nomadrussian@gmail.com>
2021-11-28 14:53:54 +0400

    Loops practice.

---
+++ Chapter_06/Exercise_02/src/main.c
@@ -0,0 +1,20 @@
+/* Exercise 2 from Chapter IV.
+ * Loops practice.
+ */
+
+#include <stdio.h>
+
+#define SIZE 5
+
+int main(void) {
+
+        for(int i = 0; i < SIZE; i++) {
+                for(int j = 0; j <= i; j++) {
+                        printf("$");
+                }
+                printf("\n");
+        }
+
+        return 0;
+}
+
